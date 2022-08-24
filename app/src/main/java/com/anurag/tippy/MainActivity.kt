f86736d0a87ea27e46055f4c332e22632de93e4c
Anurag <singh.ask@outlook.com>
2022-08-24 18:20:13 +0530

    Fixed all instantiation errors and impplemented result view!

--- app/src/main/java/com/anurag/tippy/MainActivity.kt
+++ app/src/main/java/com/anurag/tippy/MainActivity.kt
@@ -4,41 +4,38 @@ package com.anurag.tippy
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.anurag.tippy.databinding.ActivityMainBinding
+import java.sql.SQLData
 import java.text.NumberFormat
 
+class MainActivity : AppCompatActivity() {
 
- private class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        binding.CalcButton.setOnClickListener{calculateTip()}
-
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        if (::binding.isInitialized){
+            println("true")
+        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.CalcButton.setOnClickListener{ sData() }
     }
 
-    private fun calculateTip() {
-        val stringIntextField = binding.costOfService.text.toString()
-        val cost = stringIntextField.toDouble()
-
+    fun sData() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
         val tipPercentage = when (binding.radioGroupButtons.checkedRadioButtonId) {
             R.id.quality_choice_amazing -> 0.20
             R.id.quality_choice_good -> 0.18
             else -> 0.15
         }
         var tip = tipPercentage * cost
-        val roundup = binding.approxiSwitch.isChecked
-        if (roundup) {
+        val roundUp = binding.approxiSwitch.isChecked
+        if (roundUp) {
             tip = kotlin.math.ceil(tip)
         }
-      NumberFormat.getCurrencyInstance()
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 
-
 }
-
