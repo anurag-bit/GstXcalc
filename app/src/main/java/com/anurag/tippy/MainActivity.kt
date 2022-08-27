f7e4e663ec197751662b16c40ff6bf6cc26b8c26
Anurag <singh.ask@outlook.com>
2022-08-27 15:20:08 +0530

    finalised  stuff

--- app/src/main/java/com/anurag/tippy/MainActivity.kt
+++ app/src/main/java/com/anurag/tippy/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.anurag.tippy.databinding.ActivityMainBinding
 import java.text.NumberFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,22 +20,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.CalcButton.setOnClickListener{calculatetip()  }
+
     }
 
     private fun calculatetip() {
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDouble()
         val tipPercentage = when (binding.radioGroupButtons.checkedRadioButtonId) {
-            R.id.quality_choice_amazing -> 0.20
+            R.id.quality_choice_amazing -> 0.28
             R.id.quality_choice_good -> 0.18
-            else -> 0.15
+            else -> 0.12
         }
         var tip = tipPercentage * cost
         val roundUp = binding.approxiSwitch.isChecked
         if (roundUp) {
             tip = kotlin.math.ceil(tip)
         }
-        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        val formattedTip = NumberFormat.getCurrencyInstance(Locale("en", "in")).format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 
